3eb139c3cca589f6da5c4f3f5906c46793d56351
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-06 14:57:36 +0300

    fix: correct import path for Layout component in _app.tsx

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,19 +1,27 @@
-import { MovieProps } from "@/interfaces"
-import Image from "next/image"
+type MovieCardProps = {
+    title: string;
+    posterImage: string;
+    releaseYear: string | number;
+};
 
-const MovieCard: React.FC<MovieProps> = ({ title, posterImage, releaseYear }) => {
+const MovieCard: React.FC<MovieCardProps> = ({ title, posterImage, releaseYear }) => {
     return (
         <div className="h-[563px]">
             <div>
-                <Image className="h-[430px] w-full rounded-md hover:cursor-pointer" src={posterImage} width={100} height={100} alt={title} />
-
+                <Image
+                    className="h-[430px] w-full rounded-md hover:cursor-pointer"
+                    src={posterImage}
+                    width={100}
+                    height={100}
+                    alt={title}
+                />
             </div>
             <div className="flex justify-between py-4">
                 <p className="text-xl font-bold">{title}</p>
                 <p className="text-xl text-[#E2D609]">{releaseYear}</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default MovieCard
+export default MovieCard;
